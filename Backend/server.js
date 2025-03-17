27dacdd78b1d083a21660188aeb8ab3499ff972a
adnan <sirgeant10@gmail.com>
2025-03-17 23:42:22 +0600

    Change require to imports and use destination route.

--- Backend/server.js
+++ Backend/server.js
@@ -1,24 +1,27 @@
-const dotenv = require('dotenv');
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
-const userRoutes = require('./routes/userRoutes');
+import { config } from "dotenv";
+import express, { json } from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import destinationRoutes from "./routes/destinationRoutes.js";
 
 const app = express();
-app.use(express.json());
+app.use(json());
 app.use(cors());
 
-dotenv.config();
+config();
 
 const PORT = process.env.PORT || 3000;
 // connect to MongoDB
 connectDB();
 
-app.get('/', (req, res) => {
-	res.send('Welcome to Inspire API');
+app.get("/", (_req, res) => {
+  res.send("Welcome to Inspire API");
 });
 
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/destination", destinationRoutes);
+
 app.listen(PORT, () => {
-	console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
