2dc07cacce8afabf11c1c2b1a9342f4132f918e5
Sadik Sami <miasimanto@gmail.com>
2025-08-09 16:18:54 +0600

    Fixed cors origin (2)

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
 	cors({
-		origin: ['http://localhost:5173/', 'https://inspire-self.vercel.app/', 'https://inspire-dev.netlify.app/'],
+		origin: ['http://localhost:5173', 'https://inspire-self.vercel.app', 'https://inspire-dev.netlify.app'],
 		credentials: true,
 	})
 );
